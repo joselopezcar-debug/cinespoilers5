5d028b5577b7d852016664b387fe5e4e84e98464
José Alonso <jose.lopez.car@tecsup.edu.pe>
2026-04-29 17:12:30 -0500

    Create Genre for movies

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Movie, Genre  # CAMBIO
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'release_date')
+
+@admin.register(Genre)  # NUEVO
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
--- movies/models.py
+++ movies/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+class Genre(models.Model):  # NUEVO
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()  # minutos
     release_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    genres = models.ManyToManyField(Genre, related_name='movies')  # NUEVO
 
     def __str__(self):
         return self.title
